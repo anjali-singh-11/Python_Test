0555ac26eaee4b203b4f0e1247a021bb4958a505
anjali-singh-11 <anjali4628@gmail.com>
2026-06-12 14:19:40 +0530

    Numpy related question

---
+++ Day 4/num.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# numbers = np.array([1, 2, 3, 4, 5])
+
+# print(numbers * 2)
+ 
+
+# arr = np.array([
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ])
+# print(arr[0][1]) 
+
+
+marks = np.array([45,89,87,68,79])
+
+print("Total: ", np.sum(marks))
+print("Average: ", np.mean(marks))
+print("Highest: ", np.max(marks))
+print("Lowest: ", np.min(marks))
---
+++ Day 4/num1.py
@@ -0,0 +1,53 @@
+#Ques.1
+import numpy as np
+
+# arr = np.array([10, 20, 30, 40, 50])
+
+# print(arr)
+
+
+#ques2
+# arr = np.array([5, 10, 15, 20, 25])
+# print("First Element:", arr[0])
+# print("Last Element:", arr[-1])
+# print("Third Element:", arr[2])
+
+
+#Ques3
+# arr = np.array([1, 2, 3, 4, 5])
+# arr[2] = 100
+# print(arr)
+
+
+#Ques4
+# arr = np.array([11, 22, 33, 44, 55])
+# print("Length of array:", len(arr))
+
+
+#Ques6
+# arr = np.array([2, 4, 6, 8])
+# print(arr * 5)
+
+
+#Ques7
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr ** 2)
+
+
+#Ques8
+# arr = np.array([80, 70, 90, 60, 85])
+# print("Total:", np.sum(arr))
+
+
+#Ques9
+# arr = np.array([80, 70, 90, 60, 85])
+# print("Average:", np.mean(arr))
+
+
+#Ques10
+arr = np.array([45, 90, 23, 67, 88])
+print("Highest:", np.max(arr))
+print("Lowest:", np.min(arr))
+
+
+#Ques11
