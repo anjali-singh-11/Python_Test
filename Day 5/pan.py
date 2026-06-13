07a35181fd3f276960aee9d014735f488cb15f3b
anjali-singh-11 <anjali4628@gmail.com>
2026-06-13 12:34:08 +0530

    Pandas

---
+++ Day 5/pan.py
@@ -0,0 +1,14 @@
+import pandas as pd
+fifa = {
+    "players": ["Ronaldo", "Messi", "Mbappe", "Haaland"],
+    "goals": [5, 7, 6, 8]
+}
+
+df = pd.DataFrame(fifa)
+
+print("Total Goals", df["goals"].sum())
+print("Average Goals", df["goals"].mean())
+print("Highest Goals", df["goals"].max())
+
+print("\nTop Scorer:")
+print(df[df["goals"] == df["goals"].max()])
