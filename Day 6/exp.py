10375869e5e46ab181131b6d23d591a0c2eef165
anjali-singh-11 <anjali4628@gmail.com>
2026-06-14 13:02:44 +0530

    link extraction

--- Day 6/exp.py
+++ Day 6/exp.py
@@ -4,7 +4,21 @@ from bs4 import BeautifulSoup
 url = "https://www.playstation.com/en-in/ps5/"
 response = requests.get(url)
 
+# HTML ko parse kro
 soup = BeautifulSoup(response.text, "html.parser")
 
+# Title nikalo
 title = soup.title.text
 print(f"Title: {title}")
+
+h1 = soup.find("h1")
+print(f"H1: {h1.text}")
+
+p = soup.find("p")
+print(f"Paragraph: {p.text}")
+
+links = soup.findAll("a")
+
+for link in links:
+    print(link.text)
+    print(link.get)("href")
